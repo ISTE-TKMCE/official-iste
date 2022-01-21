250d059c16f66da12dbb5011b664309d2a5912dc
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-01-22 03:14:58 +0530

    block animation successfull

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,158 @@
-var p=document.querySelectorAll(".crd")
-
-p[0].addEventListener("mouseovver",function(){
-   p.classList.remove('shadow-sm')
-   p.classList.add('shadow-lg')
-})
-p[1].addEventListener("mouseover",function(){
-    // p.classList.remove('shadow-sm')
-    // p.classList.add('shadow-lg')
- })
- p[2].addEventListener("mouseover",function(){
-    console.log("working")
- })
- p[3].addEventListener("mouseover",function(){
-    console.log("working")
- })
+function animateDivb(){
+   var newq = [[95,95],[95,95],[95,185],[95,275],[5,275],[5,185],[5,275]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.b').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+             );
+           
+           }
+}
+      
+function animateDiva(){
+   var newq = [[5,5],[5,5],[5,95],[5,185],[95,185],[95,275],[185,275]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.a').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+             );
+           
+           }
+}
+function animateDivc(){
+   var newq = [[5,95],[5,95],[5,185],[95,185],[185,185],[95,185],[95,275]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.c').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+               );
+           
+           }
+}
+function animateDivd(){
+   var newq = [[5,275],[5,275],[95,275],[185,275],[275,275],[275,365],[275,275]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.d').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+               );
+           
+           }
+}
+function animateDive(){
+   var newq = [[95,275],[95,275],[275,275],[365,275],[365,185],[365,95],[365,185]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.e').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+               );
+           
+           }
+}
+function animateDivf(){
+   var newq = [[275,95],[275,95],[275,5],[185,5],[95,5],[95,95],[95,5]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.f').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+               );
+           
+           }
+}
+function animateDivg(){
+   var newq = [[275,185],[275,185],[365,185],[365,95],[365,5],[275,5],[275,95]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.g').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+               );
+           
+           }
+}
+function animateDivh(){
+   var newq = [[275,275],[275,275],[275,185],[275,95],[365,95],[275,95],[365,95]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.h').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+               );
+           
+           }
+}
+function animateDivi(){
+   var newq = [[185,5],[185,95],[275,95],[275,5],[185,5],[95,5],[185,5]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.i').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+               );
+           
+           }
+}
+function animateDivj(){
+   var newq = [[275,5],[95,5],[95,95],[95,95],[185,95],[185,185],[95,185]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.j').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+               );
+           
+           }
+}
+function animateDivk(){
+   var newq = [[365,5],[185,5],[95,5],[95,5],[95,95],[185,95],[185,95]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.k').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+               );
+           
+           }
+}
+function animateDivl(){
+   var newq = [[185,95],[185,185],[185,275],[185,185],[275,185],[275,275],[275,185]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.l').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+               );
+           
+           }
+}
+function animateDivm(){
+   var newq = [[185,185],[95,185],[185,185],[185,95],[275,95],[275,185],[185,185]];
+       for(var i=0;i<newq.length;i++){
+           
+               $('.m').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+               );
+           
+           }
+}
+                    
+setInterval(function(){
+   animateDiva()
+},200)
+setInterval(function(){
+   animateDivb()
+},200)   
+setInterval(function(){
+   animateDivc()
+},200)    
+setInterval(function(){
+   animateDivd()
+},200)   
+setInterval(function(){
+   animateDive()
+},200)    
+setInterval(function(){
+   animateDivf()
+},200)       
+setInterval(function(){
+   animateDivg()
+},200)    
+setInterval(function(){
+   animateDivh()
+},200)    
+setInterval(function(){
+   animateDivi()
+},200)  
+setInterval(function(){
+   animateDivj()
+},200)  
+setInterval(function(){
+   animateDivk()
+},200) 
+setInterval(function(){
+   animateDivl()
+},200) 
+setInterval(function(){
+   animateDivm()
+},200) 
