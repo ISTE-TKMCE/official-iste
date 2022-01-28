9486b64dad3d82e04b771adb3a30c53f42ed1316
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-01-29 04:35:06 +0530

    improved blocks ; blocks for all devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,158 +1,220 @@
-function animateDivb(){
-   var newq = [[95,95],[95,95],[95,185],[95,275],[5,275],[5,185],[5,275]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.b').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-             );
-           
-           }
+const width=window.innerWidth
+const height=window.innerHeight
+a=width*5.65/100*0.0545 //5
+ab=width*5.65/100 //95
+abc=width*5.65/100*1.95 //185
+abcd=width*5.65/100*2.9 //275
+abcde=width*5.65/100*3.84 //365
+function setsize(){
+var image1=document.getElementById('a')
+image1.style.width=(width*5.65/100*1.21).toString()+"px"
+var image2=document.getElementById('b')
+image2.style.width=(width*5.65/100*1.21).toString()+"px"
+var image3=document.getElementById('c')
+image3.style.width=(width*5.65/100*1.32).toString()+"px"
+var image4=document.getElementById('d')
+image4.style.width=(width*5.65/100*1.21).toString()+"px"
+var image5=document.getElementById('e')
+image5.style.width=(width*5.65/100*1.21).toString()+"px"
+var image6=document.getElementById('f')
+image6.style.width=(width*5.65/100*1.21).toString()+"px"
+var image7=document.getElementById('g')
+image7.style.width=(width*5.65/100*1.21).toString()+"px"
+var image8=document.getElementById('h')
+image8.style.width=(width*5.65/100*1.21).toString()+"px"
+var image9=document.getElementById('i')
+image9.style.width=(width*5.65/100*1.21).toString()+"px"
+var image10=document.getElementById('j')
+image10.style.width=(width*5.65/100*1.158).toString()+"px"
+var image11=document.getElementById('k')
+image11.style.width=(width*5.65/100*1.158).toString()+"px"
+var image12=document.getElementById('l')
+image12.style.width=(width*5.65/100*1.21).toString()+"px"
+var image13=document.getElementById('m')
+image13.style.width=(width*5.65/100*1.21).toString()+"px"
+$('.a').css('top',a.toString()+"px")
+$('.a').css('left',a.toString()+"px")
+$('.b').css('top',ab.toString()+"px")
+$('.b').css('left',ab.toString()+"px")
+$('.c').css('top',a.toString()+"px")
+$('.c').css('left',ab.toString()+"px")
+$('.d').css('top',a.toString()+"px")
+$('.d').css('left',abcd.toString()+"px")
+$('.e').css('top',ab.toString()+"px")
+$('.e').css('left',abcd.toString()+"px")
+$('.f').css('top',abcd.toString()+"px")
+$('.f').css('left',ab.toString()+"px")
+$('.g').css('top',abcd.toString()+"px")
+$('.g').css('left',abc.toString()+"px")
+$('.h').css('top',abcd.toString()+"px")
+$('.h').css('left',abcd.toString()+"px")
+$('.i').css('top',abc.toString()+"px")
+$('.i').css('left',a.toString()+"px")
+$('.j').css('top',abcd.toString()+"px")
+$('.j').css('left',a.toString()+"px")
+$('.k').css('top',abcde.toString()+"px")
+$('.k').css('left',a.toString()+"px")
+$('.l').css('top',abc.toString()+"px")
+$('.l').css('left',ab.toString()+"px")
+$('.m').css('top',abc.toString()+"px")
+$('.m').css('left',abc.toString()+"px")
 }
-      
+function animateDivb(){
+    var newq = [[ab,ab],[ab,ab],[ab,abc],[ab,abcd],[a,abcd],[a,abc],[a,abcd]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.b').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+              );
+            
+            }
+}
+       
 function animateDiva(){
-   var newq = [[5,5],[5,5],[5,95],[5,185],[95,185],[95,275],[185,275]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.a').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-             );
-           
-           }
+    var newq = [[a,a],[a,a],[a,ab],[a,abc],[ab,abc],[ab,abcd],[abc,abcd]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.a').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+              );
+            
+            }
 }
 function animateDivc(){
-   var newq = [[5,95],[5,95],[5,185],[95,185],[185,185],[95,185],[95,275]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.c').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-               );
-           
-           }
+    var newq = [[a,ab],[a,ab],[a,abc],[ab,abc],[abc,abc],[ab,abc],[ab,abcd]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.c').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+                );
+            
+            }
 }
 function animateDivd(){
-   var newq = [[5,275],[5,275],[95,275],[185,275],[275,275],[275,365],[275,275]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.d').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-               );
-           
-           }
+    var newq = [[a,abcd],[a,abcd],[ab,abcd],[abc,abcd],[abcd,abcd],[abcd,abcde],[abcd,abcd]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.d').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+                );
+            
+            }
 }
 function animateDive(){
-   var newq = [[95,275],[95,275],[275,275],[365,275],[365,185],[365,95],[365,185]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.e').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-               );
-           
-           }
+    var newq = [[ab,abcd],[ab,abcd],[abcd,abcd],[abcde,abcd],[abcde,abc],[abcde,ab],[abcde,abc]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.e').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+                );
+            
+            }
 }
 function animateDivf(){
-   var newq = [[275,95],[275,95],[275,5],[185,5],[95,5],[95,95],[95,5]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.f').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-               );
-           
-           }
+    var newq = [[abcd,ab],[abcd,ab],[abcd,a],[abc,a],[ab,a],[ab,ab],[ab,a]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.f').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+                );
+            
+            }
 }
 function animateDivg(){
-   var newq = [[275,185],[275,185],[365,185],[365,95],[365,5],[275,5],[275,95]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.g').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-               );
-           
-           }
+    var newq = [[abcd,abc],[abcd,abc],[abcde,abc],[abcde,ab],[abcde,a],[abcd,a],[abcd,ab]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.g').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+                );
+            
+            }
 }
 function animateDivh(){
-   var newq = [[275,275],[275,275],[275,185],[275,95],[365,95],[275,95],[365,95]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.h').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-               );
-           
-           }
+    var newq = [[abcd,abcd],[abcd,abcd],[abcd,abc],[abcd,ab],[abcde,ab],[abcd,ab],[abcde,ab]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.h').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+                );
+            
+            }
 }
 function animateDivi(){
-   var newq = [[185,5],[185,95],[275,95],[275,5],[185,5],[95,5],[185,5]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.i').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-               );
-           
-           }
+    var newq = [[abc,a],[abc,ab],[abcd,ab],[abcd,a],[abc,a],[ab,a],[abc,a]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.i').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+                );
+            
+            }
 }
 function animateDivj(){
-   var newq = [[275,5],[95,5],[95,95],[95,95],[185,95],[185,185],[95,185]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.j').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-               );
-           
-           }
+    var newq = [[abcd,a],[ab,a],[ab,ab],[ab,ab],[abc,ab],[abc,abc],[ab,abc]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.j').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+                );
+            
+            }
 }
 function animateDivk(){
-   var newq = [[365,5],[185,5],[95,5],[95,5],[95,95],[185,95],[185,95]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.k').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-               );
-           
-           }
+    var newq = [[abcde,a],[abc,a],[ab,a],[ab,a],[ab,ab],[abc,ab],[abc,ab]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.k').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+                );
+            
+            }
 }
 function animateDivl(){
-   var newq = [[185,95],[185,185],[185,275],[185,185],[275,185],[275,275],[275,185]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.l').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-               );
-           
-           }
+    var newq = [[abc,ab],[abc,abc],[abc,abcd],[abc,abc],[abcd,abc],[abcd,abcd],[abcd,abc]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.l').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+                );
+            
+            }
 }
 function animateDivm(){
-   var newq = [[185,185],[95,185],[185,185],[185,95],[275,95],[275,185],[185,185]];
-       for(var i=0;i<newq.length;i++){
-           
-               $('.m').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
-               );
-           
-           }
+    var newq = [[abc,abc],[ab,abc],[abc,abc],[abc,ab],[abcd,ab],[abcd,abc],[abc,abc]];
+        for(var i=0;i<newq.length;i++){
+            
+                $('.m').animate({ top: newq[i][0], left: newq[i][1] }, 1000       
+                );
+            
+            }
 }
-                    
+                   
 setInterval(function(){
-   animateDiva()
+    animateDiva()
+    setsize()
 },200)
 setInterval(function(){
-   animateDivb()
+    animateDivb()
 },200)   
 setInterval(function(){
-   animateDivc()
+    animateDivc()
 },200)    
 setInterval(function(){
-   animateDivd()
+    animateDivd()
 },200)   
 setInterval(function(){
-   animateDive()
+    animateDive()
 },200)    
 setInterval(function(){
-   animateDivf()
+    animateDivf()
 },200)       
 setInterval(function(){
-   animateDivg()
+    animateDivg()
 },200)    
 setInterval(function(){
-   animateDivh()
+    animateDivh()
 },200)    
 setInterval(function(){
-   animateDivi()
+    animateDivi()
 },200)  
 setInterval(function(){
-   animateDivj()
+    animateDivj()
 },200)  
 setInterval(function(){
-   animateDivk()
+    animateDivk()
 },200) 
 setInterval(function(){
-   animateDivl()
+    animateDivl()
 },200) 
 setInterval(function(){
-   animateDivm()
+    animateDivm()
 },200) 
